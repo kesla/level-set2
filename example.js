e52b3b7d5a30f1203fc557ca25b26ee5c0938cd0
David Björklund <david.bjorklund@gmail.com>
2014-05-03 19:21:34 +0200

    Add badge to readme

--- example.js
+++ example.js
@@ -1,12 +1,12 @@
 var db = require('level-test')()('sets')
 
-  set = require('./sets')(db)
+  set = require('./sets')()(db)
 
-set.put('key', 'value', function () {
-  set.put('key', 'value', function () {
-    set.get('key', function (err, array) {
+set.add('key', 'value', function () {
+  set.add('key', 'value', function () {
+    set.getAll('key', function (err, array) {
       console.log('key', JSON.stringify(array, null, 4))
-      set.get('bar', function (err, array) {
+      set.getAll('bar', function (err, array) {
         console.log('empty array', JSON.stringify(array, null, 4))
       })
     })
